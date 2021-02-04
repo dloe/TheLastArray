3a09eac7df7ee5d59ceb451d59d2fd1fec3b2b0e
JeremyC57 <JeremyC57@users.noreply.github.com>
2021-02-04 14:20:24 -0800

    added player movement and camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    public float offsetLimitX = 1f;
+    public float offsetLimitZ = 1f;
+    public float panSpeed = 5f;
+    private Vector3 baseOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        offset = transform.position - player.transform.position;
+        baseOffset = offset;
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = player.transform.position + offset;
+        panCamera();
+        clampOffset();
+        
+    }
+
+    private void panCamera()
+    {
+        
+
+        float xModifier = Mathf.Abs(Input.mousePosition.x - Screen.currentResolution.width / 2) / (Screen.currentResolution.width /2);
+        float zModifier = Mathf.Abs(Input.mousePosition.y - Screen.currentResolution.height / 2 ) / (Screen.currentResolution.height / 2);
+        
+
+        if (Input.mousePosition.x < (Screen.currentResolution.width / 2f) - (Screen.currentResolution.width / 6f))
+        {
+            offset.x -= xModifier * panSpeed * Time.deltaTime;
+            
+        }
+        else if (Input.mousePosition.x > (Screen.currentResolution.width / 2f) + (Screen.currentResolution.width / 6f))
+        {
+            offset.x += xModifier * panSpeed * Time.deltaTime;
+            
+        }
+        else
+        {
+            offset.x = Mathf.SmoothStep(offset.x, baseOffset.x, 8f * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y < Screen.currentResolution.height / 2f - Screen.currentResolution.height / 4f)
+        {
+            offset.z -= zModifier * panSpeed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.y > Screen.currentResolution.height / 2f + Screen.currentResolution.height / 4f)
+        {
+            offset.z += zModifier * panSpeed * Time.deltaTime;
+        }
+        else
+        {
+            offset.z = Mathf.SmoothStep(offset.z, baseOffset.z, panSpeed * 1.2f * Time.deltaTime);
+        }
+    }
+
+    private void clampOffset()
+    {
+        offset.x = Mathf.Clamp(offset.x, baseOffset.x - offsetLimitX, baseOffset.x + offsetLimitX);
+        offset.z = Mathf.Clamp(offset.z, baseOffset.z - offsetLimitZ, baseOffset.z + offsetLimitZ);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Rect rect = new Rect((Screen.currentResolution.width / 2f - Screen.currentResolution.width / 6f), Screen.currentResolution.height / 2f - Screen.currentResolution.height / 4f, (Screen.currentResolution.width / 2f + Screen.currentResolution.width / 6f) - (Screen.currentResolution.width / 2f - Screen.currentResolution.width / 6f), (Screen.currentResolution.height / 2f + Screen.currentResolution.height / 4f) - (Screen.currentResolution.height / 2f - Screen.currentResolution.height / 4f));
+        UnityEditor.Handles.BeginGUI();
+        UnityEditor.Handles.DrawSolidRectangleWithOutline(rect, Color.clear, Color.red);
+        UnityEditor.Handles.EndGUI();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //transform of the player
+    Transform _mainTransform;
+
+    Vector3 moveDir;
+    Vector3 lookDir;
+    Plane rayPlane = new Plane(Vector3.up, 0);
+
+
+    public float moveSpeed = 5f;
+    public float xLookOffset = 3f;
+    public float zLookOffset = 3f;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainTransform = transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doMovement();
+        mouseLook();
+        Debug.DrawRay(_mainTransform.position, lookDir, Color.green);
+    }
+
+    private void doMovement()
+    {
+        moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDir.Normalize();
+        moveDir *= Time.deltaTime * moveSpeed;
+        _mainTransform.Translate(moveDir, Space.World);
+
+
+    }
+
+    private void mouseLook()
+    {
+         
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        float dist;
+        if (rayPlane.Raycast(ray, out dist))
+        {
+            //if(Input.mousePosition.x)
+            lookDir = new Vector3(ray.GetPoint(dist - xLookOffset).x , _mainTransform.position.y, ray.GetPoint(dist - zLookOffset).z );
+            lookDir -= _mainTransform.position;
+
+            _mainTransform.rotation = Quaternion.Slerp(_mainTransform.rotation, Quaternion.LookRotation(lookDir), 0.15F);
+            
+           
+        }
+    }
+
+    private void rangedAttack()
+    {
+
+    }
+
+    private void meleeAttack()
+    {
+
+    }
+}
