835002f71976cefe047f5fcb0322364feafdbc9e
Dylan Loe <dylanloe@gmail.com>
2021-02-09 10:12:32 -0800

    spawns in prefabs on tiles

--- Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetsData.cs
+++ Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetsData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelAssetData", menuName = "ScritableObjects/LevelAssetData", order = 1)]
 public class LevelAssetsData : ScriptableObject
 {
-    public List<GameObject> levelAssets;
+    public List<GameObject> presetTileAssets;
 
     public List<GameObject> AssetsSpawned;
 
