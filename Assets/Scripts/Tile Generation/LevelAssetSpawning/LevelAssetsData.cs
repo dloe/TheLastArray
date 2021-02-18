3ca07351c69041bba8029857d5e4cbacf92d0c54
Dylan Loe <dylanloe@gmail.com>
2021-02-17 17:55:13 -0800

    added headers and new list to asset data obj

--- Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetsData.cs
+++ Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetsData.cs
@@ -6,15 +6,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelAssetData", menuName = "ScritableObjects/LevelAssetData", order = 1)]
 public class LevelAssetsData : ScriptableObject
 {
+    [Header("The Small 1 Tile Preset Variations")]
     public List<GameObject> presetTileAssets;
-
+    [Header("The Big 4 Tile Preset Variations")]
     public List<GameObject> presetBigTileAssets;
-
+    [Header("Refs to Assets Spawned in Scene")]
     public List<GameObject> AssetsSpawned;
-
+    [Header("Enterance Room Variations")]
     public GameObject[] EnterenceRooms;
-
+    [Header("Level Wall Prefabs")]
     public GameObject levelWall;
-
+    [Header("Tile Size")]
     public float tileSize = 100;
+    [Header("ResourcePrefabs")]
+    public List<GameObject> resourcesList;
 }
