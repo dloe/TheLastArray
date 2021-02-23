08d54ef23ffb0e47c783fb164fd5ef528cde91e3
Dylan Loe <dylanloe@gmail.com>
2021-02-23 10:25:11 -0800

    item spawn and tiles update
    - added in possible enemy spawns into forest tiles
    - started redoing item spawning system
    - added weight to possible item spawn

---
+++ Assets/PossibleItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObjectWeightType
+{
+    None,
+    Weapon,
+    Item,
+    Resource
+}
+public class PossibleItem : MonoBehaviour
+{
+    public ObjectWeightType objectWeight;
+}
--- Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetSpawn.cs
+++ Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetSpawn.cs
@@ -75,7 +75,7 @@ public class LevelAssetSpawn : MonoBehaviour
         ActivateItems();
 
         //ACTIVATE ENEMIES
-
+        ActivateEnemies();
 
     }
 
