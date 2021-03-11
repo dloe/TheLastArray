e39309f912e67422ff85f9afd7a035e78ab32e99
JeremyC57 <JeremyC57@users.noreply.github.com>
2021-03-11 11:25:04 -0800

    Added Key + Key hole, as well as converted Rifle item type and Pistol item type into one type called RangedWeapon

---
+++ Assets/Scripts/Activatables/KeyHole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHole : Activatable
+{
+    public GameObject doorObject;
+    public ItemData keyData;
+    public override void Activate()
+    {
+        if(!isActivated && Player.Instance.inventory.Contains(keyData))
+        {
+            doorObject.SetActive(false);
+            Player.Instance.inventory.RemoveItemByType(ItemType.Key);
+            isActivated = true;
+            Destroy(gameObject);
+        }
+
+        
+    }
+}
