5090d0345058eaadcccb0c22a7df9eae58cc2e56
Dylan Loe <dylanloe@gmail.com>
2021-03-30 11:12:40 -0700

    fog of war style hiding in itnerior of secret rooms works

--- Assets/SecretPresetTileInfo.cs
+++ Assets/SecretPresetTileInfo.cs
@@ -7,12 +7,4 @@ public class SecretPresetTileInfo : PresetTileInfo
     public KeyHole keyHole;
     public GameObject interior;
 
-    private void Update()
-    {
-        //if the door is opened, the inside is rendered
-        if(keyHole.isActivated)
-        {
-            interior.layer = 0;
-        }
-    }
 }
