412b54a1667ff6bdc9001f20d3b76a46b330fecf
Olah98 <aolah@mail.csuchico.edu>
2021-04-14 17:18:51 -0700

    updated droprade

--- Assets/Scripts/Enemy/OutCast.cs
+++ Assets/Scripts/Enemy/OutCast.cs
@@ -1,20 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+[Serializable]
+public struct Dropable
+{
+    public GameObject itemToDrop;
+    public int bottomChance;
+    public int topChance;
+}
 
 public class OutCast : BaseEnemy
 {
-    [Header ("Outcast items to drop")]
-    public GameObject[] itemsToDrop;
+
+    public Dropable[] dropableItem;
 
 
 
     void DropItem()
     {
-        int i = Random.Range(0, itemsToDrop.Length - 1);
-        print(itemsToDrop[i].name);
-        GameObject mine = Instantiate(itemsToDrop[i], this.gameObject.transform.position, itemsToDrop[i].transform.rotation);
-        mine.transform.rotation.y.Equals(0);
+        int i = UnityEngine.Random.Range(0, 100);
+        print(i);
+        GameObject temp = null;
+        for (int t = 0; t <= dropableItem.Length - 1; t++)
+        {
+            temp = returnItem(dropableItem[t], i);
+            if (temp != null)
+                break;
+        }
+        if (temp != null)
+        {
+            Instantiate(temp, this.gameObject.transform.position, temp.transform.rotation);
+            
+        }
+    }
+
+    GameObject returnItem(Dropable temp,int i)
+    {
+        GameObject drop = null;
+        for(int e = temp.bottomChance; e <= temp.topChance; e++)
+        {
+            
+            if (i == e)
+            { 
+                return temp.itemToDrop;
+            }
+        }
+        return drop;
     }
 
     public override void OnDeath()
