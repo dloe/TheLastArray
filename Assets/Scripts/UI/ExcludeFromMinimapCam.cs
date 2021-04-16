6efd03f57de55cba21dd541cc210b36aaebaf514
Dylan Loe <dylanloe@gmail.com>
2021-04-16 14:46:53 -0700

    updates to walls and minimap

---
+++ Assets/Scripts/UI/ExcludeFromMinimapCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcludeFromMinimapCam : MonoBehaviour
+{
+    private Camera cam;
+    public LayerMask toExclude;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+
+        cam.cullingMask = ~toExclude;
+       // foreach (LayerMask layer in toExclude)
+        //{
+       //     cam.cullingMask += ~layer;
+       // }
+    }
+
+}
