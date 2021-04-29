391fffbd160e5edd6676b57817e73cd9f110d3ba
Dylan Loe <dylanloe@gmail.com>
2021-04-29 09:41:06 -0700

    fixed null ref

--- Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetSpawn.cs
+++ Assets/Scripts/Tile Generation/LevelAssetSpawning/LevelAssetSpawn.cs
@@ -172,7 +172,7 @@ public class LevelAssetSpawn : MonoBehaviour
                 //SPAWN PLAYER
                 play = Instantiate(playerPref, Vector3.zero, playerSpawn.transform.rotation);
                 //Debug.Log(play);
-                play.transform.GetChild(1).GetComponent<Player>().PlayerCamRot = playerSpawn.transform.eulerAngles.y;
+                play.transform.GetChild(2).GetComponent<Player>().PlayerCamRot = playerSpawn.transform.eulerAngles.y;
                 //Debug.Log("Player Spawn set");
                 StartCoroutine(setPlayerPosition(play, playerSpawn.transform.position));
 
